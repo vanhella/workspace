740e8751ec258db858956fbb5d3919445f033d9e
cox.trevor2012@gmail.com <soto@vanhella.(none)>
2013-11-16 16:42:24 -0600

    Yeah

---
+++ c/char.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+        int c;
+        c=getchar();
+        while((c=getchar()) != EOF){
+                putchar(c); 
+        }
+        return 0;
+}
---
+++ c/variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+int a = 3;
+float b = 4.5;
+double c = 5.25;
+double sum;
+
+sum = a + b + c;
+
+printf("The sum of a, b, and c is %.2f.\n", sum);
+return 0;
+}
